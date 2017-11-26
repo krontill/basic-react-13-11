36d6f986fbd2b56f88a7752afe79c1acbc77a3bb
krontill <till.work@gmail.com>
2017-11-26 22:23:10 +0300

    Add react-day-picker.

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,34 @@ import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 class App extends Component {
     state = {
-        selected: null
-    }
+        selected: null,
+        from: undefined,
+        to: undefined,
+    };
+
+    handleDayClick = (day) => {
+      const range = DateUtils.addDayToRange(day, this.state);
+      this.setState(range);
+    };
+
+	handleResetClick = () => {
+		this.setState({
+      from: undefined,
+      to: undefined,
+    });
+	};
 
     handleSelect = selected => this.setState({ selected })
 
     render() {
-        const {articles} = this.props
+        const {articles} = this.props;
+        const {from, to} = this.state;
+	      const modifiers = { start: from, end: to };
         const options = articles.map(article => ({
             label: article.title,
             value: article.id
@@ -21,6 +39,25 @@ class App extends Component {
         return (
             <div>
                 <h1>App name</h1>
+              <p>
+		            {!from && !to && 'Please select the first day.'}
+		            {from && !to && 'Please select the last day.'}
+		            {from &&
+		            to &&
+		            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+		            {from &&
+		            to && (
+                  <button className="link" onClick={this.handleResetClick}>
+                    Reset
+                  </button>
+		            )}
+              </p>
+                <DayPicker
+                  className="Selectable"
+                  selectedDays={[from, { from, to }]}
+                  modifiers={modifiers}
+                  onDayClick={this.handleDayClick}/>
                 <UserForm />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleSelect} multi />
                 <ArticleList articles = {articles} defaultOpenId = {articles[0].id}/>
